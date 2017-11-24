ce3e171bf4f683fd47266af0da4d7414bcdf3115
Denis Brodbeck <denis@shahaya.com>
2017-11-24 10:23:30 +0100

    Update doc

--- primitive.go
+++ primitive.go
@@ -7,6 +7,17 @@ import (
 	"github.com/nfnt/resize"
 )
 
+// Primitive generates a SVG consisting of several simple shapes
+// that approximate the main features visible inside the image.
+//
+// All primitive generation related flags are supported.
+// See https://github.com/fogleman/primitive for a list of supported flags
+// and their meaning in respect to the output.
+//
+// You should seed your rng in your main function, before calling this function.
+//
+// Try:
+//   rand.Seed(time.Now().UTC().UnixNano())
 func Primitive(input image.Image, workSize, outputSize, count, mode, alpha, repeat, workers int, background string) (svg string, err error) {
 	// scale down input image if needed
 	if workSize > 0 {
